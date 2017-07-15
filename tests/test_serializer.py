2746d6ba68dfb8f9c33162f8005e7dd562269493
thyagostall <thstall@gmail.com>
2017-07-15 00:56:14 -0300

    Create snippeds of serializer

---
+++ tests/test_serializer.py
@@ -0,0 +1,56 @@
+import json
+from decimal import Decimal
+
+from prudget.account import Account
+from prudget.dashboard import Dashboard
+
+
+class DashboardEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Dashboard):
+            result = {}
+            return result
+        elif isinstance(obj, Account):
+            result = {'name': obj.name, 'balance': obj.balance}
+            return result
+        elif isinstance(obj, Decimal):
+            return str(obj)
+        else:
+            return super().default(obj)
+
+
+def serialize_dashboard(dashboard):
+    return json.dumps(dashboard, cls=DashboardEncoder)
+
+
+def serialize_account(account):
+    return json.dumps(account, cls=DashboardEncoder)
+
+
+def test_serialize_empty_dashboard():
+    dashboard = Dashboard()
+    result = serialize_dashboard(dashboard)
+
+    assert result == '{}'
+
+
+def test_serialize_account():
+    account = Account('Some account')
+    account.deposit(100)
+
+    result = serialize_account(account)
+    result = json.loads(result)
+
+    assert result == {'balance': '100', 'name': 'Some account'}
+
+
+# def test_serialize_dashboard_with_one_account():
+#     account = Account('Some account')
+#     account.deposit(100)
+#
+#     dashboard = Dashboard()
+#     dashboard.add_account(account)
+#
+#     result = serialize_dashboard(dashboard)
+#
+#     assert result == '{"accounts": [{"name":"Some account","balance":100}]}'
