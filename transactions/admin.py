61c379b450c45dd0901ef9fe8946f28a6018b54d
thyagostall <thstall@gmail.com>
2017-11-19 01:47:59 -0200

    Fill user in automatically

--- transactions/admin.py
+++ transactions/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from transactions.models import Account, Bucket, Currency
 
 
-admin.site.register(Account, admin.ModelAdmin)
-admin.site.register(Bucket, admin.ModelAdmin)
-admin.site.register(Currency, admin.ModelAdmin)
+class LoggedUserModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.exclude = ('owner',)
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request)
+        return query_set.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.owner = request.user
+        except AttributeError:
+            pass
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Account, LoggedUserModelAdmin)
+admin.site.register(Bucket, LoggedUserModelAdmin)
+admin.site.register(Currency, LoggedUserModelAdmin)
