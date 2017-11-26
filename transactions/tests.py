d9d969e5a3978a862f880c3c93a332531c6faa38
thyagostall <thstall@gmail.com>
2017-11-25 23:36:28 -0200

    Ensure only user transactions are shown

--- transactions/tests.py
+++ transactions/tests.py
@@ -59,3 +59,23 @@ class TransactionsViewTestCase(TestCase):
 
         self.assertTrue(logged_successfully)
         self.assertEqual(response.status_code, 200)
+
+    def test_with_user_should_not_shot_others_users_transactions(self):
+        first_user = create_user('first', 'first@email.com', 'password')
+        second_user = create_user('second', 'second@email.com', 'password')
+
+        first_account = create_account(first_user, 'Itaú', 'BRL')
+        second_account = create_account(second_user, 'Itaú', 'BRL')
+
+        first_amount = Decimal('129.99')
+        second_amount = Decimal('-75.77')
+        create_transaction(first_user, first_account, amount=first_amount)
+        create_transaction(second_user, second_account, amount=second_amount)
+
+        logged_successfully = self.client.login(username='first', password='password')
+
+        response = self.client.get(reverse('transactions'))
+
+        self.assertTrue(logged_successfully)
+        self.assertContains(response, '129.99')
+        self.assertNotContains(response, '-75.77')
