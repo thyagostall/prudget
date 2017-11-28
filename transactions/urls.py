a6e41751e81538faa292d687269ce671da55585f
thyagostall <thstall@gmail.com>
2017-11-27 23:03:23 -0200

    Create dashboard and convert to views to FBVs

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
 
 from transactions import views
-from transactions.views import TransactionsView, LoginView, AccountListView
 
 urlpatterns = [
-    url(r'^transactions/', TransactionsView.as_view(), name='transactions'),
-    url(r'^account-list/', AccountListView.as_view(), name='account_list'),
-    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^login/', views.login, name='login'),
     url(r'^dashboard/', views.dashboard, name='dashboard'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,53 +1,30 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView as AuthLoginView
 from django.shortcuts import render
-from django.views.generic import FormView
-from django.views.generic import TemplateView
 
-from transactions import transactions
 from transactions.forms import TransactionForm
 from transactions.models import Transaction, Account
+from transactions.transactions import create_group_id
 
 
-class TransactionsView(LoginRequiredMixin, FormView):
-    login_url = '/finance/login/'
-
-    form_class = TransactionForm
-    template_name = 'transactions/transactions.html'
-    success_url = '.'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['transactions'] = Transaction.objects.filter(owner=self.request.user).order_by('-id')
-        return context
-
-    def get_form_kwargs(self):
-        form_kwargs = super().get_form_kwargs()
-        form_kwargs.update({
-            'user': self.request.user
-        })
-        return form_kwargs
-
-    def form_valid(self, form):
-        transaction = form.save(commit=False)
-        transaction.owner = self.request.user
-        transaction.group_id = transactions.create_group_id('COMMON')
-        transaction.save()
-        return super().form_valid(form)
-
-
-class LoginView(AuthLoginView):
-    template_name = 'transactions/login.html'
-
-
-class AccountListView(LoginRequiredMixin, TemplateView):
-    login_url = '/finance/login/'
-    template_name = 'transactions/account_list.html'
-
-    def get_context_data(self, **kwargs):
-        accounts = Account.objects.filter(owner=self.request.user)
-        return dict(accounts=accounts)
+def login(request):
+    return render(request, 'transactions/login.html')
 
 
 def dashboard(request):
-    return render(request, 'transactions/dashboard.html')
+    form = TransactionForm(request.user, request.POST or None)
+
+    if form.is_valid():
+        transaction = Transaction(**form.cleaned_data)
+        transaction.owner = request.user
+        transaction.group_id = create_group_id('COMMON')
+        transaction.save()
+
+    transactions = Transaction.objects.filter(owner=request.user).order_by('-id')
+    accounts = Account.objects.filter(owner=request.user)
+
+    context = {
+        'transactions': transactions,
+        'accounts': accounts,
+        'form': form,
+    }
+    return render(request, 'transactions/dashboard.html', context=context)
