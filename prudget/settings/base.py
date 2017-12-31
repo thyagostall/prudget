61367dcda32cb52f97890a56cac376ea49d3f70c
thyagostall <thstall@gmail.com>
2017-12-31 12:58:32 -0200

    Move templates to root app directory

--- prudget/settings/base.py
+++ prudget/settings/base.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'prudget.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
