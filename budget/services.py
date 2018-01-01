8ac628d550ae064f562bf63e2ea365b9809e7d2d
thyagostall <thstall@gmail.com>
2018-01-01 19:36:24 -0200

    Paid expenses

--- budget/services.py
+++ budget/services.py
@@ -1,4 +1,6 @@
-from django.db.models import Sum
+import datetime
+
+from django.db.models import Sum, Q
 
 from transactions.models import Bucket
 
@@ -17,3 +19,41 @@ def sum_expenses_amounts(expense_queryset):
 
 def calculate_profit_or_loss(total_income, total_buckets, total_expenses):
     return total_income - (total_buckets + total_expenses)
+
+
+def was_expense_paid(expense):
+    from transactions.models import Transaction
+
+    current_date = datetime.datetime.today()
+
+    if expense.periodicity == expense.MONTHLY:
+        return Transaction.objects. \
+            filter(owner=expense.owner) \
+            .filter(expense=expense) \
+            .filter(date__year=current_date.year) \
+            .filter(date__month=current_date.month) \
+            .exists()
+    elif expense.periodicity == expense.BIMONTHLY:
+        previous_month_date = previous_month_from_date(current_date)
+        return Transaction.objects. \
+            filter(owner=expense.owner) \
+            .filter(expense=expense) \
+            .filter((
+                        Q(date__month=current_date.month) &
+                        Q(date__year=current_date.year)
+                    ) | (
+                        Q(date__month=previous_month_date.month) &
+                        Q(date__year=previous_month_date.year)
+                    )) \
+            .exists()
+    else:
+        return Transaction.objects. \
+            filter(owner=expense.owner) \
+            .filter(expense=expense) \
+            .filter(date__year=current_date.year) \
+            .exists()
+
+
+def previous_month_from_date(current_date):
+    current_date = current_date.replace(day=1)
+    return current_date - datetime.timedelta(days=1)
