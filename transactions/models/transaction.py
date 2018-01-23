1b5567f82413b8b47c2f48bd543299624dc7aca4
thyagostall <thstall@gmail.com>
2018-01-23 00:03:23 -0200

    Remove legacy reference_date

--- transactions/models/transaction.py
+++ transactions/models/transaction.py
@@ -6,7 +6,6 @@ from transactions.models import UserModel, Account, Bucket
 class Transaction(UserModel):
     description = models.CharField(max_length=50)
     date = models.DateField()
-    reference_date = models.DateField(blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     account = models.ForeignKey(Account, blank=True, null=True, on_delete=models.DO_NOTHING)
     bucket = models.ForeignKey(Bucket, blank=True, null=True, on_delete=models.DO_NOTHING)
