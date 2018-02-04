aea8699ebffa19c4fec169bd0d2aec9316b31ec3
thyagostall <thstall@gmail.com>
2018-02-04 19:22:40 -0200

    Remove group id column

--- core/models/transaction.py
+++ core/models/transaction.py
@@ -9,4 +9,3 @@ class Transaction(UserModel):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     account = models.ForeignKey(Account, blank=True, null=True, on_delete=models.DO_NOTHING)
     bucket = models.ForeignKey(Bucket, blank=True, null=True, on_delete=models.DO_NOTHING)
-    group_id = models.CharField(max_length=50)
