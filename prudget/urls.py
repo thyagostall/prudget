19cfe49f4d70bdc146f47fd966214d380dc71d6d
thyagostall <thstall@gmail.com>
2018-02-04 19:44:47 -0200

    Installed django debug toolbar

--- prudget/urls.py
+++ prudget/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from prudget import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
