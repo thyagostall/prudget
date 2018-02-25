048c84973c504f8cac3556e0e3b7e852034e62b4
thyagostall <thstall@gmail.com>
2018-02-24 23:33:53 -0300

    Remove account field for bucket transfers

--- core/forms/transfer_to_bucket_form.py
+++ core/forms/transfer_to_bucket_form.py
@@ -1,19 +1,23 @@
+from datetime import datetime
+
 from django import forms
 from django.core.validators import MinValueValidator
+from core.forms import DateInput
 
 import core.services
-from core.forms import TransactionForm
 from core.models import Bucket, Transaction
 
 
-class TransferToBucketForm(TransactionForm):
+class TransferToBucketForm(forms.ModelForm):
+    date = forms.DateField(widget=DateInput(), initial=datetime.today())
     amount = forms.DecimalField(validators=[MinValueValidator(0)])
     destination_bucket = forms.ModelChoiceField(queryset=Bucket.objects.none())
 
     def __init__(self, user, *args, **kwargs):
-        super().__init__(user, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.user = user
         self.fields['destination_bucket'].queryset = Bucket.objects.filter(owner=self.user)
+        self.fields['bucket'].queryset = Bucket.objects.filter(owner=self.user)
 
     def save(self, commit=True):
         self.instance.owner = self.user
@@ -25,4 +29,4 @@ class TransferToBucketForm(TransactionForm):
 
     class Meta:
         model = Transaction
-        fields = ['description', 'date', 'amount', 'bucket', 'account', 'destination_bucket']
+        fields = ['description', 'date', 'amount', 'bucket', 'destination_bucket']
