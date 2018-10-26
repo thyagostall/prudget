acd63294f2465d3f392681a169e43d3890181b01
Thyago Stall <thstall@gmail.com>
2018-10-25 23:37:59 -0300

    Adds pagination to dashboard

--- core/views/dashboard_view.py
+++ core/views/dashboard_view.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from core import services
@@ -7,13 +8,18 @@ from core.models import Transaction, Bucket, Account
 
 @login_required
 def dashboard(request):
-    transactions = Transaction.objects.filter(owner=request.user).order_by('-date', '-id').select_related('bucket', 'account')
+    all_transactions = Transaction.objects.filter(owner=request.user).order_by('-date', '-id').select_related('bucket', 'account')
 
     buckets = list(filter(lambda bucket: bucket.balance() != 0, Bucket.objects.filter(owner=request.user)))
     accounts = list(filter(lambda bucket: bucket.balance() != 0, Account.objects.filter(owner=request.user)))
 
     bucket_total = services.get_query_set_balance(buckets)
     account_total = services.get_query_set_balance(accounts)
+
+    paginator = Paginator(all_transactions, 50)
+
+    page = request.GET.get('p', 1)
+    transactions = paginator.get_page(page)
 
     context = {
         'transactions': transactions,
