5606ecb9a28dbe1a3782f33818c3d265f27b57c4
Thyago Stall <thstall@gmail.com>
2018-11-04 23:00:13 -0200

    Removing pagination code from dashboard

--- core/views/dashboard_view.py
+++ core/views/dashboard_view.py
@@ -1,7 +1,6 @@
 from functools import reduce
 
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from core.models import Transaction
@@ -10,18 +9,13 @@ from core.services import account_balance_queryset, bucket_balance_queryset
 
 @login_required
 def dashboard(request):
-    all_transactions = Transaction.objects.filter(owner=request.user).order_by('-date', '-id').select_related('bucket', 'account')
+    transactions = Transaction.objects.filter(owner=request.user).order_by('-date', '-id').select_related('bucket', 'account')
 
     buckets = list(filter(lambda bucket: bucket.current_balance != 0, bucket_balance_queryset(request)))
     accounts = list(filter(lambda account: account.current_balance != 0, account_balance_queryset(request)))
 
     bucket_total = reduce(lambda accumulator, bucket: bucket.current_balance + accumulator, buckets, 0)
     account_total = reduce(lambda accumulator, account: account.current_balance + accumulator, accounts, 0)
-
-    paginator = Paginator(all_transactions, 50)
-
-    page = request.GET.get('p', 1)
-    transactions = paginator.get_page(page)
 
     context = {
         'transactions': transactions,
