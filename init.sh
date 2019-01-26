e4ac9da4576402ed58e177931cfc8353da12f4e0
Thyago Stall <thstall@gmail.com>
2019-01-25 23:38:28 -0200

    Using wait-for-it to synchronize postgres

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+./wait-for-it.sh db:5432 -t 300
 sleep 10
+
+echo "Postgres is up"
+
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 python manage.py runserver 0.0.0.0:8000
