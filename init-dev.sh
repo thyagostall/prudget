93980f36b054ef0ad8901f7512af663a0c494135
Thyago Stall <thstall@gmail.com>
2019-01-26 14:07:40 -0200

    Start using Green Unicorn

--- init-dev.sh
+++ init-dev.sh
@@ -7,4 +7,4 @@ echo "Postgres is up"
 
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
-python manage.py runserver 0.0.0.0:8000
+gunicorn -b 0.0.0.0:8000 prudget.wsgi --log-file -
